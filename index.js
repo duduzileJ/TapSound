ad8ee3426ca1779387fdd72db71e1ebf323a61ad
Duduzile <98821469+duduzileJ@users.noreply.github.com>
2023-03-06 18:57:14 +0200

    Sounds
    added sounds

---
+++ index.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelectorAll("visual");
+    const colors = [
+        "#3484ad",
+        "#440af1",
+        "#188712"
+    ];
+
+    pads.forEach((pad, index)=>{
+        pad.addEventListener('click',function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+
+    //function that makes bubbles
+    const createBubbles = (index) =>{
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    };
+});
